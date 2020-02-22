1c9ab8777d9256dc846940c12ee22b5331252bbe
xMokAx <ahmedmokhtar11j@gmail.com>
2020-02-22 09:10:28 +0200

    feature: the ability to build the project with the right firebase config using env-cmd and two .env files(.env.development, .env.production)

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,29 +6,16 @@ import { User } from "../actions/user";
 import { Room, Message } from "../actions/chatRooms";
 import { store } from "..";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-let config = isProduction
-  ? {
-      apiKey: process.env.REACT_APP_PRODUCTION_API_KEY,
-      authDomain: process.env.REACT_APP_PRODUCTION_AUTH_DOMAIN,
-      databaseURL: process.env.REACT_APP_PRODUCTION_DATABASE_URL,
-      projectId: process.env.REACT_APP_PRODUCTION_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_PRODUCTION_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_PRODUCTION_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_PRODUCTION_APP_ID,
-      measurementId: process.env.REACT_APP_PRODUCTION_MEASUREMENT_ID
-    }
-  : {
-      apiKey: process.env.REACT_APP_API_KEY,
-      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-      databaseURL: process.env.REACT_APP_DATABASE_URL,
-      projectId: process.env.REACT_APP_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_APP_ID,
-      measurementId: process.env.REACT_APP_MEASUREMENT_ID
-    };
+let config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 firebase.initializeApp(config);
 
